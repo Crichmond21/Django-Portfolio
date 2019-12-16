41479eb52cca0b6119c43a68fa84eceafe4e1f3b
Carter Richmond <Cman5687@gmail.com>
2019-12-15 21:00:49 -0500

    Initial Commit

---
+++ portfolio/jobs/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Job(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='images/')
+    summary = models.CharField(max_length=300)
+    status = models.CharField(max_length=20)
+    link = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.title
